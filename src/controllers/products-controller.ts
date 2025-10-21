4e0aed2dca8b7434615b110ae964d4241ce0a646
2305ray <rayssagarcia035@gmail.com>
2025-10-20 22:37:12 -0300

    feat: iniciando o projeto

---
+++ src/controllers/products-controller.ts
@@ -0,0 +1,13 @@
+import { AppError } from "@/utils/AppError";
+import { NextFunction, Request, Response } from "express";
+
+export class ProductController {
+    async index(request: Request, response: Response, next: NextFunction) {
+        try {
+
+            return response.json({ message: "ok" })
+        } catch (error) {
+            next(error)
+        }
+    }
+}
