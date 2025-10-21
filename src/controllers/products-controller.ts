78636456d925ad0be2dace90b9deb63827c73bf1
2305ray <rayssagarcia035@gmail.com>
2025-10-21 00:22:13 -0300

    criando zod e adicionando produt no banco de dados

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,11 +1,32 @@
+import { knexConfig } from "@/database/knex";
 import { AppError } from "@/utils/AppError";
 import { NextFunction, Request, Response } from "express";
+import z from "zod";
 
 export class ProductController {
     async index(request: Request, response: Response, next: NextFunction) {
         try {
 
             return response.json({ message: "ok" })
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async create(request: Request, response: Response, next: NextFunction) {
+
+        try {
+            const bodySchema = z.object({
+                name: z.string().trim().min(6),
+                price: z.number().gt(0, { message: "value must be greater than 0" }),
+            })
+
+            const { name, price } = bodySchema.parse(request.body)
+
+            await knexConfig<ProductRepository>("products").insert({
+                name, price
+            })
+            return response.status(201).json()
         } catch (error) {
             next(error)
         }
