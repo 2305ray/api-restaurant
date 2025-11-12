c91607dbecd98f39d92a6d7a7f9d1f2153469de4
2305ray <rayssagarcia035@gmail.com>
2025-11-12 11:31:50 -0300

    feat: calculando a conta e exportando requisições

--- src/routes/orders-routes.ts
+++ src/routes/orders-routes.ts
@@ -5,6 +5,8 @@ const ordersRoutes = Router()
 const ordersController = new OrdersController()
 
 ordersRoutes.post("/", ordersController.create)
-ordersRoutes.get("/table-session/:table_session_id", ordersController.index)
+ordersRoutes.get("/table-session/:table_session_id", ordersController.index) //listar muitos registros
+ordersRoutes.get("/table-session/:table_session_id/total", ordersController.show)//mostrar registro específico
+
 
 export { ordersRoutes }
